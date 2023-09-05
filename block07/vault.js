1ac7cbeb0bbf69052668561c58dc360383538525
Solomon Ferdin <solomonferdin@gmail.com>
2023-09-05 16:35:31 -0700

    First Commit

---
+++ block07/vault.js
@@ -0,0 +1,13 @@
+// not sure how i will complete this assignment
+let x = 9 + 1
+let y = 39 + 1
+let z = 40 - 1
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+console.log(message)
+
+function fun() {
+
+
+    alert (`${message}\n ${x}, ${y}, ${z}`);
+ }
